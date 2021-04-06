50d8a611782b4386e0faddc67f34c437e0d389f0
Edwald He <git config --global user.email you@example.com>
2021-04-07 09:38:05 +1000

    finished

---
+++ client/src/setupTests.js
@@ -0,0 +1,13 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+/*
+    location /sockjs-node{
+        proxy_pass http://client;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+    }
+*/
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom";
